b2af94f79f8c88191c2d0403fbac8105a8558672
Anton <antonjan@users.noreply.github.com>
2024-04-30 21:27:29 +0200

    Create Pdf2focx.py

---
+++ Pdf2focx.py
@@ -0,0 +1,20 @@
+#pip install pdf2docx
+# Importing the Converter() class
+from pdf2docx import Converter
+
+# Specifying the pdf & docx files
+pdf_file = 'muscles.pdf'
+docx_file = 'muscles.docx'
+
+
+try:
+    # Converting PDF to Docx
+    cv_obj = Converter(pdf_file)
+    cv_obj.convert(docx_file)
+    cv_obj.close()
+
+except:
+    print('Conversion Failed')
+    
+else:
+    print('File Converted Successfully')
