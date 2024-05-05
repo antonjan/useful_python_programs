4b4695d4119d905b36fc428f9b8d2bef1221f912
Anton <antonjan@users.noreply.github.com>
2024-05-05 21:10:45 +0200

    Create word_cloud.py

---
+++ word_cloud.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from wordcloud import Wordcloud
+text= 'I love Python Coding because Python is easy to learn and simple
+wordcloud = WordCloud(width=800, height=400).generate(text)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.title('Word Tree')
+plt.show()
+#clcoding.com
