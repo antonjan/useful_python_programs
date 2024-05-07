47af7c7fb1db71849f74c7fb79710a5d1b3bcd25
Anton <antonjan@users.noreply.github.com>
2024-05-07 16:58:27 +0200

    Create data_to_chsrt.py

---
+++ data_to_chsrt.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4']
+values = [80, 20, 50, 30]
+fig, ax1 = plt.subplots()
+ax1.bar(categories, values, color='skyblue')
+ax1.set_ylabel('Values', color='skyblue')
+ax1.tick_params('y, colors='skyblue')
+ax2= ax1.twinx()
+cumulative_values = [sum(values[:i+1]) for i in range(len(values))]
+ax2.plot(categories, cumulative_values, color='red', marker='o')
+ax2.set_ylabel('Cumulative Values', color='red')
+ax2.tick_params('y', colors='red')
+plt.title("Pareto Chart')
+plt.show()
+#clcoding.com
