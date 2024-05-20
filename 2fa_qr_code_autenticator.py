3a22bbef7161c9f57f760231b51921ad9fd4511c
Anton Janovsky <anton.janovsky@gmail.com>
2024-05-20 14:50:43 +0200

    adding qr code authjetication

--- 2fa_qr_code_autenticator.py
+++ 2fa_qr_code_autenticator.py
@@ -1,8 +1,10 @@
+#pip3 install pyotp
+#pip3 install qrcodehis program 
+#This program creates a top.png barcode image that you can scan with google aut or authey apps
 import time
 import pyotp
 import qrcode
-key= NeuralNineMySuperSecretKey"
-uri = pyotp.totp.TOTP(key).provisioning_uri(name="MikeSmith123"
-issuer_name="NeuralNine App")
+key= "NeuralNineMySuperSecretKey"
+uri = pyotp.totp.TOTP(key).provisioning_uri(name="MikeSmith123",issuer_name="NeuralNine App")
 print(uri)
-qrcode.make(uri).save("totp.png"2
+qrcode.make(uri).save("totp.png")
