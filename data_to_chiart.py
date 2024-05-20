97ad9f1d5299bc46674d4f488a707a0a50eb3884
Anton Janovsky <anton.janovsky@gmail.com>
2024-05-20 14:56:48 +0200

    fixing error

--- data_to_chiart.py
+++ data_to_chiart.py
@@ -4,12 +4,12 @@ values = [80, 20, 50, 30]
 fig, ax1 = plt.subplots()
 ax1.bar(categories, values, color='skyblue')
 ax1.set_ylabel('Values', color='skyblue')
-ax1.tick_params('y, colors='skyblue')
+ax1.tick_params('y', colors='skyblue')
 ax2= ax1.twinx()
 cumulative_values = [sum(values[:i+1]) for i in range(len(values))]
 ax2.plot(categories, cumulative_values, color='red', marker='o')
 ax2.set_ylabel('Cumulative Values', color='red')
 ax2.tick_params('y', colors='red')
-plt.title("Pareto Chart')
+plt.title('Pareto Chart')
 plt.show()
 #clcoding.com
