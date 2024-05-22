d47bf7126ede4620af22e7f4af9f682247bc5f96
Anton Janovsky <anton.janovsky@gmail.com>
2024-05-22 15:03:53 +0200

    adding updates to face regonition

--- 2fa_qr_code_autenticator.py
+++ 2fa_qr_code_autenticator.py
@@ -5,6 +5,6 @@ import time
 import pyotp
 import qrcode
 key= "NeuralNineMySuperSecretKey"
-uri = pyotp.totp.TOTP(key).provisioning_uri(name="MikeSmith123",issuer_name="NeuralNine App")
+uri = pyotp.totp.TOTP(key).provisioning_uri(name="MikeSmith123",issuer_name="Giga Technology App")
 print(uri)
 qrcode.make(uri).save("totp.png")
