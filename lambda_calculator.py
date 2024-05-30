dc808749ab811e0464a66992b8921486f982fcf4
Anton <antonjan@users.noreply.github.com>
2024-05-30 20:30:26 +0200

    Create lambda_calculator.py

---
+++ lambda_calculator.py
@@ -0,0 +1,8 @@
+# Creating a simple calculator with Lambda functi
+calculator
+'add': lambda x, y: x + y,
+subtract': lambda x, y: x - y,
+multiply': lambda x, y: x * y,
+'divide': lambda x, y: x / y,
+result = calculator['multiply'](4, 5)
+print(result) # Output: 20
