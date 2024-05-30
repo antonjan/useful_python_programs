e7f6f750220d31e27d149d5932c8bf4dbdb5310e
Anton <antonjan@users.noreply.github.com>
2024-05-30 20:08:35 +0200

    Create url_shortner.py

---
+++ url_shortner.py
@@ -0,0 +1,6 @@
+import pyshorteners
+long_url = input("Enter the URL to shorten:")
+type_tiny = pyshorteners.Shortener()
+short _url = type_tiny.tinyurl.short(long_url)
+# Display the shortened URL
+print("The Shortened URL is:short_url)
