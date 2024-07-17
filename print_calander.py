cccca80ec01388b6ed6942e6e04b6ed9ae74c930
Anton <antonjan@users.noreply.github.com>
2024-07-17 19:55:05 +0200

    Create print_calander.py

---
+++ print_calander.py
@@ -0,0 +1,7 @@
+from calendar import*
+year = int(input('Enter Year:'))
+print(calendar(year, 2, 1, 8, 2))
+#2 = 2 characters for days (Mo, Tu, etc)
+#1 = 1 line (row) for each week
+#8 = 8 rows for each month 
+#3 3 columns for all months of the year
